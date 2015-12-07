723a69dafc9645062c573210c2be462cc83b55e6
LinxiaHu <chunyun@mail.ustc.edu.cn>
2015-12-07 18:02:02 +0800

    init

---
+++ src/com/hd/sys/OSmoudle.py
@@ -0,0 +1,12 @@
+'''
+Created on Dec 7, 2015
+
+@author: hadoop
+'''
+import os as oos
+
+# if __name__ == '__main__':
+#     pass
+
+print(oos.listdir(oos.getcwd()))
+print(oos.path.exists("/usr/local"))
---
+++ src/com/hd/sys/Other.py
@@ -0,0 +1,6 @@
+# coding=utf-8
+
+import os
+import sys
+print "hello from other.py"
+print "中国，德国，日本" #要支持中文必须在文件开始时（前2行）指定编码
